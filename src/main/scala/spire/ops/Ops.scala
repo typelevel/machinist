d084b1cd4f77e8771e2f250ddc7d951c2fe8de06
Erik Osheim <d_m@plastic-idolatry.com>
2014-03-07 00:38:41 -0500

    make comments not cause scaladoc warnings

--- src/main/scala/spire/ops/Ops.scala
+++ src/main/scala/spire/ops/Ops.scala
@@ -12,161 +12,154 @@ import scala.reflect.macros.Context
  */
 trait Ops {
 
-  /**
-   * Given context, this method rewrites the tree to call the desired
-   * method with the lhs parameter. We find the symbol which is
-   * applying the macro and use its name to determine what method to
-   * call.
-   *
-   * If we see code like:
-   *
-   *   -lhs
-   *
-   * After typing and implicit resolution, we get trees like:
-   *   
-   *   conversion(lhs)(ev).unary_-(): R
-   *
-   * The macro should produce trees like:
-   *
-   *   ev.negate(lhs): R
-   */
+  // Given context, this method rewrites the tree to call the desired
+  // method with the lhs parameter. We find the symbol which is
+  // applying the macro and use its name to determine what method to
+  // call.
+  //
+  // If we see code like:
+  //
+  //   -lhs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs)(ev).unary_-(): R
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.negate(lhs): R
+  //
   def unop[R](c: Context)(): c.Expr[R] = {
     import c.universe._
     val (ev, lhs) = unpack(c)
     c.Expr[R](Apply(Select(ev, findMethodName(c)), List(lhs)))
   }
 
-  /**
-   * Like unop, but with ev provided to the method instead of to the
-   * implicit constructor.
-   * 
-   * If we see code like:
-   * 
-   *   lhs.abs
-   * 
-   * After typing and implicit resolution, we get trees like:
-   *   
-   *   conversion(lhs).abs(ev: Ev): R
-   *
-   * The macro should produce trees like:
-   *
-   *   ev.abs(lhs): R
-   */
+  // Like unop, but with ev provided to the method instead of to the
+  // implicit constructor.
+  //
+  // If we see code like:
+  //
+  //   lhs.abs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs).abs(ev: Ev): R
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.abs(lhs): R
+  //
   def unopWithEv[Ev, R](c: Context)(ev: c.Expr[Ev]): c.Expr[R] = {
     import c.universe._
     val lhs = unpackWithoutEv(c)
     c.Expr[R](Apply(Select(ev.tree, findMethodName(c)), List(lhs)))
   }
 
-  /**
-   * Given context and an expression, this method rewrites the tree to
-   * call the "desired" method with the lhs and rhs parameters. We
-   * find the symbol which is applying the macro and use its name to
-   * determine what method to call.
-   *
-   * If we see code like:
-   *
-   *   lhs + rhs
-   *
-   * After typing and implicit resolution, we get trees like:
-   *   
-   *   conversion(lhs)(ev).$plus(rhs: A): R
-   *
-   * The macro should produce trees like:
-   *
-   *   ev.method(lhs: A, rhs: A): R
-   */
+  // Given context and an expression, this method rewrites the tree to
+  // call the "desired" method with the lhs and rhs parameters. We
+  // find the symbol which is applying the macro and use its name to
+  // determine what method to call.
+  //
+  // If we see code like:
+  //
+  //   lhs + rhs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs)(ev).$plus(rhs: A): R
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.method(lhs: A, rhs: A): R
+  //
   def binop[A, R](c: Context)(rhs: c.Expr[A]): c.Expr[R] = {
     import c.universe._
     val (ev, lhs) = unpack(c)
     c.Expr[R](Apply(Select(ev, findMethodName(c)), List(lhs, rhs.tree)))
   }
 
-  /**
-   * Like binop, but for right-associative operators (eg. +:).
-   * 
-   * If we see code like:
-   * 
-   *   lhs *: rhs
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(rhs)(ev).$times$colon(lhs)
-   * 
-   * The macro should produce trees like:
-   * 
-   *   ev.timesl(lhs, rhs)
-   */
+  // Like binop, but for right-associative operators (eg. +:).
+  //
+  // If we see code like:
+  //
+  //   lhs *: rhs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(rhs)(ev).$times$colon(lhs)
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.timesl(lhs, rhs)
+  //
   def rbinop[A, R](c: Context)(lhs: c.Expr[A]): c.Expr[R] = {
     import c.universe._
     val (ev, rhs) = unpack(c)
     c.Expr[R](Apply(Select(ev, findMethodName(c)), List(lhs.tree, rhs)))
   }
 
-  /**
-   * Like binop, but with ev provided to the method instead of to the
-   * implicit constructor.
-   * 
-   * If we see code like:
-   * 
-   *   lhs % rhs
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(lhs).$percent(rhs)(ev)
-   * 
-   * The macro should produce trees like:
-   * 
-   *   ev.mod(lhs, rhs)
-   */
+  // Like binop, but with ev provided to the method instead of to the
+  // implicit constructor.
+  //
+  // If we see code like:
+  //
+  //   lhs % rhs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs).$percent(rhs)(ev)
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.mod(lhs, rhs)
+  //
   def binopWithEv[A, Ev, R](c: Context)(rhs: c.Expr[A])(ev: c.Expr[Ev]): c.Expr[R] = {
     import c.universe._
     val lhs = unpackWithoutEv(c)
     c.Expr[R](Apply(Select(ev.tree, findMethodName(c)), List(lhs, rhs.tree)))
   }
 
-  /**
-   * Like rbinop, but with ev provided to the method instead of to the
-   * implicit constructor.
-   * 
-   * If we see code like:
-   * 
-   *   lhs *: rhs
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(rhs).$times$colon(lhs)(ev)
-   * 
-   * The macro should produce trees like:
-   * 
-   *   ev.timesl(lhs, rhs)
-   */
+  // Like rbinop, but with ev provided to the method instead of to the
+  // implicit constructor.
+  //
+  // If we see code like:
+  //
+  //   lhs *: rhs
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(rhs).$times$colon(lhs)(ev)
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.timesl(lhs, rhs)
+  //
   def rbinopWithEv[A, Ev, R](c: Context)(lhs: c.Expr[A])(ev: c.Expr[Ev]): c.Expr[R] = {
     import c.universe._
     val rhs = unpackWithoutEv(c)
     c.Expr[R](Apply(Select(ev.tree, findMethodName(c)), List(lhs.tree, rhs)))
   }
 
-  /**
-   * Combine an implicit enrichment with a lifting method.
-   * 
-   * If we see code like:
-   * 
-   *   lhs + 1
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(lhs)(ev0).$plus(1)(ev1): R
-   * 
-   * The macro should produce trees like:
-   * 
-   *   ev0.plus(lhs, ev1.fromInt(1))
-   * 
-   * In Spire, this let's us use Ring's fromInt method and
-   * ConvertableTo's fromDouble (etc.) before applying an
-   * op. Eventually, we should generalize the way we choose the
-   * lifting method.
-   */
+  // Combine an implicit enrichment with a lifting method.
+  //
+  // If we see code like:
+  //
+  //   lhs + 1
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs)(ev0).$plus(1)(ev1): R
+  //
+  // The macro should produce trees like:
+  //
+  //   ev0.plus(lhs, ev1.fromInt(1))
+  //
+  // In Spire, this let's us use Ring's fromInt method and
+  // ConvertableTo's fromDouble (etc.) before applying an
+  // op. Eventually, we should generalize the way we choose the
+  // lifting method.
+  //
   def binopWithLift[A: c.WeakTypeTag, Ev, R](c: Context)(rhs: c.Expr[A])(ev1: c.Expr[Ev]): c.Expr[R] = {
     import c.universe._
     val (ev0, lhs) = unpack(c)
@@ -175,22 +168,21 @@ trait Ops {
     c.Expr[R](Apply(Select(ev0, findMethodName(c)), List(lhs, rhs1)))
   }
 
-  /**
-   * This is like binopWithLift, but we use the same evidence
-   * parameter to make the method call and do the lifting.
-   * 
-   * If we see code like:
-   * 
-   *   lhs * 2
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(lhs)(ev).$times(2): R
-   * 
-   * The macro should produce trees like:
-   * 
-   *   ev.times(lhs, ev.fromInt(2))
-   */
+  // This is like binopWithLift, but we use the same evidence
+  // parameter to make the method call and do the lifting.
+  //
+  // If we see code like:
+  //
+  //   lhs * 2
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs)(ev).$times(2): R
+  //
+  // The macro should produce trees like:
+  //
+  //   ev.times(lhs, ev.fromInt(2))
+  //
   def binopWithSelfLift[A: c.WeakTypeTag, Ev, R](c: Context)(rhs: c.Expr[A]): c.Expr[R] = {
     import c.universe._
     val (ev0, lhs) = unpack(c)
@@ -199,34 +191,32 @@ trait Ops {
     c.Expr[R](Apply(Select(ev0, findMethodName(c)), List(lhs, rhs1)))
   }
 
-  /**
-   * Similar to binop, but for situations where there is no evidence
-   * parameter, and we just want to call a method on the rhs.
-   * 
-   * After typing and implicit resolution, we get trees like:
-   * 
-   *   conversion(lhs).foo(rhs)
-   * 
-   * and we want to get out:
-   * 
-   *   rhs.foo(lhs)
-   */
+  // Similar to binop, but for situations where there is no evidence
+  // parameter, and we just want to call a method on the rhs.
+  //
+  // After typing and implicit resolution, we get trees like:
+  //
+  //   conversion(lhs).foo(rhs)
+  //
+  // and we want to get out:
+  //
+  //   rhs.foo(lhs)
+  //
   def flip[A, R](c: Context)(rhs: c.Expr[A]): c.Expr[R] = {
     import c.universe._
     val lhs = unpackWithoutEv(c)
     c.Expr[R](Apply(Select(rhs.tree, findMethodName(c)), List(lhs)))
   }
 
-  /**
-   * Given context, this method pulls the 'ev' and 'lhs' values out of
-   * instantiations of implicit -Ops classes.
-   * 
-   * For instance, given a tree like:
-   * 
-   *   new FooOps(x)(ev)
-   * 
-   * This method would return (ev, x).
-   */
+  // Given context, this method pulls the 'ev' and 'lhs' values out of
+  // instantiations of implicit -Ops classes.
+  //
+  // For instance, given a tree like:
+  //
+  //   new FooOps(x)(ev)
+  //
+  // This method would return (ev, x).
+  //
   def unpack[T[_], A](c: Context) = {
     import c.universe._
     c.prefix.tree match {
@@ -236,16 +226,15 @@ trait Ops {
     }
   }
 
-  /**
-   * Given context, this method pulls the 'lhs' value out of
-   * instantiations of implicit -Ops classes.
-   * 
-   * For instance, given a tree like:
-   * 
-   *   new FooOps(x)
-   * 
-   * This method would return x.
-   */
+  // Given context, this method pulls the 'lhs' value out of
+  // instantiations of implicit -Ops classes.
+  //
+  // For instance, given a tree like:
+  //
+  //   new FooOps(x)
+  //
+  // This method would return x.
+  //
   def unpackWithoutEv(c: Context) = {
     import c.universe._
     c.prefix.tree match {
@@ -275,15 +264,13 @@ trait Ops {
     newTermName(operatorNames.getOrElse(s, s))
   }
 
-  /**
-   * Map of symbolic -> textual name conversions.
-   * 
-   * If this map is empty, the macros will not do any special
-   * rewriting and all names will be passed through.
-   * 
-   * Symbolic names should be written as Scala would represent them
-   * internally. For example, + should be written as $plus.
-   */
+  // Map of symbolic -> textual name conversions.
+  //
+  // If this map is empty, the macros will not do any special
+  // rewriting and all names will be passed through.
+  //
+  // Symbolic names should be written as Scala would represent them
+  // internally. For example, + should be written as $plus.
   def operatorNames: Map[String, String]
 }
 
